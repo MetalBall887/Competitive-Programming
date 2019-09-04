6c45c8af876ecac62a81ca986d817c794bd3e25b
MetalBall887 <30705065+MetalBall887@users.noreply.github.com>
2019-09-04 15:36:26 +0200

    Atcoder D - Rotation Sort

--- AtCoder/AtCoder032-AGC-D.cpp
+++ AtCoder/AtCoder032-AGC-D.cpp
@@ -3,12 +3,12 @@
 #include <stdio.h>
 //Important insight: if we place our elements on coordinate line, we can view the shift as changing place of one element.
 //A is for changing to the left, B - to the right. Whatever the coordinates of two elements are, it's always possible to put
-//another one between them. Now we can add baskets. We can put our elements in baskets without changing the order. There are
+//another one between them. Now we can add baskets. We put our elements in baskets without changing the order. There are
 //n baskets. After all operations the element with number i should be in i-th basket. If we put in every basket one element
 //we'll get a worst-case O(n) algorithm. But we can do this: |_| |_(2)_| |_(3)_| |_(1)(4)_| |_(5)_|. This way, we'll need only
-//one operation, when |_(2)_| |_(3)_| |_(1)_| |_(4)_| |_(5)_| here it will be three. Also it will be more optimal to put 
-//the element on the other side of corresponing box. To find the best way, we can do d[i][j] - the cost of sorting i first
-//elements into first j baskets.
+//one operation, when |_(2)_| |_(3)_| |_(1)_| |_(4)_| |_(5)_| here it will be three. Also it will be sometimes more optimal 
+//to put the element on the other side of corresponing box. To find the best way, we can do d[i][j] - the cost of sorting i 
+//first elements into first j baskets.
 #include <algorithm>
 #include <cstring>
 #include <cstdlib>
